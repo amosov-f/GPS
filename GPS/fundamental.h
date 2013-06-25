c783735bed4f4970d168f1af50cec239eeb45a0a
afaf <amosov.f@mail.ru>
2013-06-25 11:44:10 +0400

    Namespace with fundamental constants of Earth

---
+++ GPS/fundamental.h
@@ -0,0 +1,12 @@
+#ifndef FUNDAMENTAL_H
+#define FUNDAMENTAL_H
+
+namespace Fundamental {
+    const long double mu = 3986004418.0L * 100000.0L;
+    const long double velOmega_e = 7.2921151467e-05L;
+    const long double c = 299792458.0L;
+    const long double a = 6378137.0L;
+    const long double b = 6356752.0L;
+}
+
+#endif // FUNDAMENTAL_H
